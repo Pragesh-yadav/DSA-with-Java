7b096c2818a00173df19cbb05c3f44d6c5f48802
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-28 23:37:54 +0530

    Check even and Odd using Ternary Operator

--- 00_basics/EvenOdd.java
+++ 00_basics/EvenOdd.java
@@ -4,11 +4,15 @@ public class EvenOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        if(x % 2 == 0){
-            System.out.println("Even");
-        }
-        else{
-            System.out.println("Odd");
-        }
+
+        String ans = x%2 == 0 ? "Even": "Odd";
+        System.out.println(ans);
+        // if(x % 2 == 0){
+        //     System.out.println("Even");
+        // }
+        // else{
+        //     System.out.println("Odd");
+        // }
+        
     }
 }
